d2d9c1ab1f9c12d2dde9925dc7f3fab9502a3b08
Marco Munizaga <git@marcopolo.io>
2021-12-13 13:01:08 -0800

    GH Action

--- flake.nix
+++ flake.nix
@@ -3,32 +3,40 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { system = system; };
-      in
-      {
-        packages.ipldChecker =
-          pkgs.buildGoModule rec {
-            pname = "ipld-schema-playground";
-            version = "0.0.1";
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs { system = system; };
+        in
+        {
+          packages.ipldChecker =
+            pkgs.buildGoModule rec {
+              pname = "ipld-schema-playground";
+              version = "0.0.1";
 
-            src = ./.;
+              src = ./.;
 
-            vendorSha256 = pkgs.lib.fakeSha256;
+              vendorSha256 = "sha256-unJdAXNrDeFQFtwPHje437AFv7A8X42r74F/k3nZ1u0=";
 
-            runVend = true;
+              buildPhase = "
+                GOOS=js GOARCH=wasm go build -o main.wasm
+              ";
+              installPhase = "
+                cp -r www $out
+                cp main.wasm $out/main.wasm
+              ";
 
-            meta = with pkgs.lib; {
-              description = "A IPLD Schema playground";
-              homepage = "https://github.com/marcopolo/go-ipld-schema-playground";
-              license = licenses.mit;
-              platforms = platforms.linux ++ platforms.darwin;
+              meta = with pkgs.lib;
+                {
+                  description = "A IPLD Schema playground";
+                  homepage = "https://github.com/marcopolo/go-ipld-schema-playground";
+                  license = licenses.mit;
+                  platforms = platforms.linux ++ platforms.darwin;
+                };
             };
+          defaultPackage = self.packages.${system}.ipldChecker;
+          devShell = pkgs.mkShell {
+            buildInputs = [ pkgs.go pkgs.simple-http-server ];
           };
-        defaultPackage = self.packages.${system}.ipldChecker;
-        devShell = pkgs.mkShell {
-          buildInputs = [ pkgs.go pkgs.simple-http-server ];
-        };
-      });
+        });
 }
